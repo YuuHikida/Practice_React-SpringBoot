31ae4c293df5f893b9a80a5594e20c03ad876a08
Yuu Hikida <ponponda0103@gmail.com>
2024-06-25 01:47:10 +0900

    起動完了mybatisのverの不具合でした,mapping errorはあり

--- src/main/java/com/example/react_SpringBoot_practice/ReactSpringBootPracticeApplication.java
+++ src/main/java/com/example/react_SpringBoot_practice/ReactSpringBootPracticeApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.example.react_SpringBoot_practice.mapper")
 public class ReactSpringBootPracticeApplication {
 
 	public static void main(String[] args) {
